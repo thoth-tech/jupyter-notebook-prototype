2942b0f08958d636aafaaebc138957a6fcdc1543
Ethan Keirs <60351308+EthanKeirs@users.noreply.github.com>
2022-05-01 13:19:47 +1000

    Add files via upload

---
+++ jupyter-notebook-prototype-ui/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core'
+import { RouterModule } from '@angular/router'
+import { BrowserModule } from '@angular/platform-browser'
+
+import { ComponentsModule } from './components/components.module'
+import { AppComponent } from './app.component'
+
+const routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomeModule),
+  },
+]
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes), ComponentsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
