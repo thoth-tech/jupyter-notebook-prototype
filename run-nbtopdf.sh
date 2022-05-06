62ebe7891387f9bfd784816697a7fae8e59c823f
Lachlan Foy <lachie.foy7@gmail.com>
2022-05-06 17:52:47 +1000

    copy output out of container in bash script

--- run-nbtopdf.sh
+++ run-nbtopdf.sh
@@ -1,1 +1,3 @@
-docker run --rm nbtopdf bash -c "jupyter nbconvert --to pdf input.ipynb --output=output.pdf"
+docker run --name nbtopdf-container nbtopdf bash -c "jupyter nbconvert --to pdf input.ipynb --output=output.pdf" # run convert command through new container created from nbtopdf image
+docker cp nbtopdf-container:convert/output.pdf output.pdf # copy the resulting output pdf
+docker rm nbtopdf-container # remove container
