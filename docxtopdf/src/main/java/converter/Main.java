b6e59ab299f60cf8dc755afab27199fee1bda98b
Lachlan Foy <lachie.foy7@gmail.com>
2022-05-18 16:57:07 +1000

    fix java formatting

--- docxtopdf/src/main/java/converter/Main.java
+++ docxtopdf/src/main/java/converter/Main.java
@@ -4,31 +4,23 @@ import org.apache.poi.xwpf.converter.pdf.PdfConverter;
 import org.apache.poi.xwpf.converter.pdf.PdfOptions;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 
-
 import java.io.*;
 import java.util.Scanner;
 
-
 public class Main {
-
-
     public static void main(String[] args) throws IOException {
+        Scanner runCommandScanner = new Scanner(System.in);
+        String runCommand = runCommandScanner.nextLine();
 
-            Scanner runCommandScanner = new Scanner(System.in);
-            String runCommand = runCommandScanner.nextLine();
+        //put all filenames in inputFile into array list
+        String[] inputFileNames = new File("input").list();
 
-
-            //put all filenames in inputFile into array list
-            String[] inputFileNames = new File("input").list();
-
-            if (inputFileNames != null){
-
-                //Removes .docx from file name, and converts it to pdf.
-                for (int i = 0; i < inputFileNames.length; i++) {
-
-                    try (
-                            InputStream is = new FileInputStream(new File("input/" + inputFileNames[i]));
-                            OutputStream out = new FileOutputStream(new File("output/" + inputFileNames[i].substring(0, inputFileNames[i].length()-5) + ".pdf"));
+        if (inputFileNames != null) {
+        //Removes .docx from file name, and converts it to pdf.
+            for (int i = 0; i < inputFileNames.length; i++) {
+                try (
+                    InputStream is = new FileInputStream(new File("input/" + inputFileNames[i]));
+                    OutputStream out = new FileOutputStream(new File("output/" + inputFileNames[i].substring(0, inputFileNames[i].length()-5) + ".pdf"));
                     ) {
                         // 1) Load DOCX into XWPFDocument
                         XWPFDocument document = new XWPFDocument(is);
@@ -42,21 +34,13 @@ public class Main {
                         e.printStackTrace();
                     }
                 }
-                int outputFolderLength = new File("output").list().length;
-                String outputMessage = outputFolderLength == inputFileNames.length ?
-                        "0" :
-                        "102";
-                System.out.println(outputMessage);
-            } else {
-                System.out.println("101");
-            }
 
-
-            }
+            int outputFolderLength = new File("output").list().length;
+            String outputMessage = outputFolderLength == inputFileNames.length ? "0" : "102";
+            System.out.println(outputMessage);
+            
+        } else {
+            System.out.println("101");
         }
-
-
     }
-
-
 }
