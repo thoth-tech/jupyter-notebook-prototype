a217e38cef9a9fd8c88b7de115564c4d8f065fe1
Jordan Litsas <jordanlitsas@live.com.au>
2022-05-19 11:00:30 +1000

    changed to single file conv, filepath fixed with data folder

--- docxtopdf/src/main/java/converter/Main.java
+++ docxtopdf/src/main/java/converter/Main.java
@@ -9,38 +9,29 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello World"); // test 
-
-        Scanner runCommandScanner = new Scanner(System.in);
-        String runCommand = runCommandScanner.nextLine();
-
         //put all filenames in inputFile into array list
-        String[] inputFileNames = new File("input").list();
-
-        if (inputFileNames != null) {
-        //Removes .docx from file name, and converts it to pdf.
-            for (int i = 0; i < inputFileNames.length; i++) {
-                try (
-                    InputStream is = new FileInputStream(new File("input/" + inputFileNames[i]));
-                    OutputStream out = new FileOutputStream(new File("output/" + inputFileNames[i].substring(0, inputFileNames[i].length()-5) + ".pdf"));
-                    ) {
-                        // 1) Load DOCX into XWPFDocument
-                        XWPFDocument document = new XWPFDocument(is);
-                        // 2) Prepare Pdf options
-                        PdfOptions options = PdfOptions.create();
-                        // 3) Convert XWPFDocument to Pdf
-                        PdfConverter.getInstance().convert(document, out, options);
-
-                        System.out.println("File " + inputFileNames[i] + " was converted.");
-                    } catch (Throwable e) {
-                        e.printStackTrace();
-                    }
-                }
-
-            int outputFolderLength = new File("output").list().length;
-            String outputMessage = outputFolderLength == inputFileNames.length ? "0" : "102";
+        File inputFile = new File("./tmp/data/input.docx");
+        if (inputFile != null) {
+            //Removes .docx from file name, and converts it to pdf.
+            try (
+                    InputStream is = new FileInputStream(inputFile);
+                    OutputStream out = new FileOutputStream(new File("./tmp/data/output.pdf"));
+            ) {
+                // 1) Load DOCX into XWPFDocument
+                XWPFDocument document = new XWPFDocument(is);
+                // 2) Prepare Pdf options
+                PdfOptions options = PdfOptions.create();
+                // 3) Convert XWPFDocument to Pdf
+                PdfConverter.getInstance().convert(document, out, options);
+            } catch (Throwable e) {
+                e.printStackTrace();
+            }
+            //Program converts one file at a time, so the data folder should have one file pre-conversion and two post-conversion.
+            //Verifies there is one extra file in the data folder.
+            //See README for output codes
+            int dataFolderLength = new File("./tmp/data").list().length;
+            String outputMessage = dataFolderLength == 2 ? "0" : "1";
             System.out.println(outputMessage);
-
         } else {
             System.out.println("101");
         }
